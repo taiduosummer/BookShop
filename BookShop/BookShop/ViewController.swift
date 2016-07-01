464e6ad8e7d00ed188ec529ddd8e02e8ef7cd26d
chenpeng <chenpeng@chinanet.local>
2016-07-01 18:20:13 +0800

    2016 07 01

---
+++ BookShop/BookShop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookShop
+//
+//  Created by chenpeng on 16/7/1.
+//  Copyright © 2016年 summer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
