ff6b616f75cd61f339a971ca4da8edc28ce48f93
chenpeng <chenpeng@ChinaNet.local>
2016-07-04 10:43:24 +0800

    api

---
+++ BookShop/BookShop/apiConfig/apiConfig.swift
@@ -0,0 +1,14 @@
+//
+//  apiConfig.swift
+//  BookShop
+//
+//  Created by chenpeng on 16/7/4.
+//  Copyright © 2016年 summer. All rights reserved.
+//
+
+import Foundation
+
+let appKey = "f23af073335545fb10687916b266d3af"
+
+let appAPI = "http://japi.juhe.cn/book/recommend.from"
+
