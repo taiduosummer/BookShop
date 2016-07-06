6bb747204052e55054290ac1c2ce332652aa1417
chenpeng <chenpeng@ChinaNet.local>
2016-07-06 09:33:56 +0800

    ..

--- BookShop/BookShop/ViewController.swift
+++ BookShop/BookShop/ViewController.swift
@@ -7,15 +7,31 @@
 //
 
 import UIKit
+import SnapKit
+import Alamofire
 
 class ViewController: UIViewController {
 
-    
+    var segmentBook:UISegmentedControl!
+    var myTabelView:UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        segmentBook = UISegmentedControl(items: ["今日推荐","点击排行","新书推荐"])
+        segmentBook.selectedSegmentIndex = 0
+        view.addSubview(segmentBook)
+        segmentBook.snp.makeConstraints { (make) in
+            make.top.equalTo(view.snp.top).offset(64)
+            make.left.right.equalTo(view)
+            make.height.equalTo(30)
+        }
     }
 
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
